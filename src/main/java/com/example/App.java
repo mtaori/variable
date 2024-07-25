bcd28cce9766488a20f4b29cbd89f343a0e4c67a
Manan Taori <taorimanan19@gmail.com>
2024-07-25 21:32:57 +0530

    Update App.java in Staging-branch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Feature Branch 1!");
+        System.out.println("Staging Branch 2!");
     }
 }
